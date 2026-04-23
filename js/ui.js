334ed3830f5abb71b1a20eb87c574bf53d6674ec
PelosiAndre <andrerpelosi@gmail.com>
2026-04-22 21:29:09 -0300

    style: add tailwind, lucide e identidade visual

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,21 @@
 /*INDEX.HTML*/
 
+window.tailwind = {
+    config: {
+        theme: {
+            extend: {
+                colors: {
+                    primary: '#22C345',
+                    darkGray: '#333333',
+                },
+                fontFamily: {
+                    sans: ['Inter', 'sans-serif'],
+                }
+            }
+        }
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function() {
     if (typeof lucide !== 'undefined') {
         lucide.createIcons();
